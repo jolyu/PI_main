b129ba0074cdab7cd2b52fc7392fcf4308d75b2d
Ludvik Rønning <lro@shifthyperloop.com>
2020-02-24 10:50:37 +0100

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+import cv2
+import imgProsessing
+import sensors 
+import imgProsessing
+
+def setup():
+    #setUpLogFile()
+    #willYouBeWifi()  set up telemetry link
+    #setupSensors()   set up sensor stuff
+    #setUpCamera()    set up camera link?
+    #setupImgProsessing() set up img prosessing (slå sammen med den over?)
+
+def main():
+    #camera()?
+    #prosessFrame()? slå sammen med den over?
+    #sensorStuff() 
+    #transmitData()? 
+    #reciveveData()? (slå sammen med den over?)
+
+if __name__=="__main__":
+    main()
