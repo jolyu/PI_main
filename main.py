e9180977e5f5c76acdbec0c8877ff5bbe2dbedb9
sevatm <55739219+sevatm@users.noreply.github.com>
2020-03-05 10:31:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import cv2
 import image_prosessing_main as ip
 import logging_setup as log
+import setup_database as db
 import time
 
 def setup():                                   #setup function
     log.initLogging()                                #setUpLogFile()sets up log file
     log.info('Initializing....')                     #log message
     detector = ip.initImgProsessing()                #initialize blob detector
-    return detector
+    db_ref = db.setupDatabase()                      #setup database
+    return detector, db_ref
 
 def main():
-    detector = setup()                          #setup function
+    detector, db_ref = setup()                          #setup function
     log.info('Setup completed')
     log.info('Now running main')
 
