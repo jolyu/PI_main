779db2ea440bacc6e2003f7f161151d1389d3a01
sevatm <55739219+sevatm@users.noreply.github.com>
2020-03-09 11:09:32 +0100

    Update telemetry.py

--- telemetry.py
+++ telemetry.py
@@ -3,6 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import db
 from firebase_admin import auth
 import logging as log
+from datetime import datetime
 
 def setupDatabase():
     cred = credentials.Certificate('key.json')
@@ -16,12 +17,14 @@ def setupDatabase():
 
 def transmit(db_ref, numBlobs, sensorStuff):
         try:
+            
             data = {
                 'time': datetime.timestamp(datetime.now()),
                 'birds': numBlobs,
             }
-        
-            db_ref.child(data['time']).set(data)
+            dateTimeObj = datetime.now()
+            timestampStr = dateTimeObj.strftime("%H%M%S%f%b%d%Y")
+            db_ref.child(timestampStr).set(data)
             log.info('Uploaded to database')
         except:
             log.critical('Could not upload to database')
