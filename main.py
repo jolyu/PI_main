72d3133e20402ba62d21c7b552b1d510b47d76fc
Ludvik Rønning <lro@shifthyperloop.com>
2020-03-09 13:24:15 +0100

    fix spacing

--- main.py
+++ main.py
@@ -19,26 +19,26 @@ def main():
     tel.transmit(db_ref, 1000, 200)
 
     #cv2.namedWindow("preview")
-vc = cv2.VideoCapture(0)
+    vc = cv2.VideoCapture(0)
 
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
+    if vc.isOpened(): # try to get the first frame
+        rval, frame = vc.read()
+    else:
+        rval = False
 
-while rval:
+    while rval:
 
-    cv2.imshow("preview", frame)
+        cv2.imshow("preview", frame)
 
-    rval, frame = vc.read()
-    keypoints = ip.detectStuff(frame, detector)
-    print('number of blobs ', len(keypoints))
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
-        break
+        rval, frame = vc.read()
+        keypoints = ip.detectStuff(frame, detector)
+        print('number of blobs ', len(keypoints))
+        key = cv2.waitKey(20)
+        if key == 27: # exit on ESC
+            break
 
-cv2.destroyAllWindow()
-vc.release()
+    cv2.destroyAllWindow()
+    vc.release()
 
     
 if __name__=="__main__":
