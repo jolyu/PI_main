1915366a4690106660e9c38ac512cc2df5660698
SakariaHAli <46136295+SakariaHAli@users.noreply.github.com>
2020-03-09 12:54:10 +0100

    Update Tracking.py

--- Tracking.py
+++ Tracking.py
@@ -4,7 +4,9 @@ def KeypointsToBoxes(keypoints):
     boxes = []
     for keypoint in keypoints:
         point = keypoint.pt
-        size = keypoint.size()
-        box = (point[0],point[1],size,size)
+        size = int(keypoint.size())
+        box = (int(point[0])- (size/2),int(point[1]) - (size/2),size,size)
         boxes.append(box)
     return boxes
+
+print("ssessss")
