6089ad4eaf0359bdb216bde0b83183e760819b7b
SakariaHAli <46136295+SakariaHAli@users.noreply.github.com>
2020-03-09 11:30:43 +0100

    Update Blob_detect_main.py

--- Blob_detect_main.py
+++ Blob_detect_main.py
@@ -1,55 +1,59 @@
 import cv2
 import numpy as np
-import extra_greyscaling
+import extra_greyscaling as grey
 import logging_setup as log
 import time
 
 def initBlobDetector():
-     # Setup SimpleBlobDetector parameters.
+    # Setup SimpleBlobDetector parameters.
     params = cv2.SimpleBlobDetector_Params()
 
     # Change thresholds
-    params.minThreshold = 5
-    params.maxThreshold = 70
+    params.minThreshold = 0
+    params.maxThreshold = 40
+
 
     # Filter by Area.
     params.filterByArea = True
-    params.minArea = 30
+    params.minArea = 100
 
     # Filter by Circularity
     params.filterByCircularity = True
-    params.minCircularity = 0.01
+    params.minCircularity = 0.1
 
     # Filter by Convexity
     params.filterByConvexity = True
-    params.minConvexity = 0.7
+    params.minConvexity = 0.87
 
     # Filter by Inertia
-    params.filterByInertia = False
-    params.minInertiaRatio = 0.05
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.01
 
 
     #determine cv version
-    print(cv2.__version__)
     if cv2.__version__.startswith('2'):
         detector = cv2.SimpleBlobDetector(params)
     else:
         detector = cv2.SimpleBlobDetector_create(params)
-    
     return detector
 
 def readImage(img):
-    # read image
-    img = cv2.imread(img, cv2.IMREAD_COLOR)
+    # read image in greyscale 
+    img = cv2.imread(img,cv2.IMREAD_GRAYSCALE)            
+                                                    #put fancy G2B algorithm here 
+    img = cv2.resize(img, (650,500))
     img = 255-img
     log.info('Reading image...')
+    #img = img[40:470, 0:610]
     return img
     
-def detect(img, detector):
+def detectStuff(img, detector):
     # detect suff
-    keypoints = detector.detect(img)
+    keyPoints = detector.detect(img)
     log.info('Detecting keypoints...')
-    return keypoints
+    #draw detected keypoints as red circles
+    #imgKeyPoints = cv2.drawKeypoints(img, keypoints, np.array([]),(0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS) #uncomment for testing
+    return keyPoints
 
 def initImgProsessing():
     detector = initBlobDetector()
@@ -57,6 +61,8 @@ def initImgProsessing():
     return detector
 
 if __name__ == "__main__":
-    print('not main')
-    
-        
+    img = readImage('FLIR0028.jpg')
+    detector = initBlobDetector()
+    keypoints = detectStuff(img,detector)
+    for keypoint in keypoints:
+        print(type(keypoint.pt))
