19477c3763011ca4250faa5c1c07734fc563f226
SakariaHAli <46136295+SakariaHAli@users.noreply.github.com>
2020-03-09 15:15:51 +0100

    Added tracking and base program flow

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import image_prosessing_main as ip
 import logging_setup as log
 import telemetry as tel
 import time
-
+import Tracking as track
 
 def setup():                                   #setup function
     log.initLogging()                                #setUpLogFile()sets up log file
     log.info('Initializing....')                     #log message
     detector = ip.initImgProsessing()                #initialize blob detector
     db_ref = tel.setupDatabase()                      #setup database
+    trackerType = "KCF"
+    multiTracker = cv2.MultiTracker_create()
+    birds = 0
     return detector, db_ref
 
 def main():
@@ -17,7 +20,6 @@ def main():
     log.info('Setup completed.')
     log.info('Now running main.')
     tel.transmit(db_ref, 1000, 200)
-
     #cv2.namedWindow("preview")
     vc = cv2.VideoCapture(0)
 
@@ -27,14 +29,18 @@ def main():
         rval = False
 
     while rval:
-        cv2.imshow("preview", frame)
         rval, frame = vc.read()
+        success, boxes = multiTracker.update(frame)
         keypoints = ip.detectStuff(frame, detector)
-        print('number of blobs ', len(keypoints))
-        key = cv2.waitKey(20)
-        if key == 27: # exit on ESC
-            break
+        newKeypoints = track.removeTrackedBlobs(keypoints,boxes)
+        if(not len(newKeypoints==0)):
+            birds = birds + len(newKeypoints)
+            newBoxes = track.KeypointsToBoxes(newKeypoints)
+            for box in newKeypoints:
+                multiTracker.add(tf.createTrackerByName(trackerType), frame, newBox)
+        print(birds)
 
+        
     cv2.destroyAllWindow()
     vc.release()
 
