c7c955774f734d25d12f6c0dc0c66f4909763a9d
Ludvik Rønning <lro@shifthyperloop.com>
2020-03-09 09:44:08 +0100

    push push så får du en suss

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import cv2
 import image_prosessing_main as ip
 import logging_setup as log
-import setup_database as db
+import telemetry as tel
 import time
 
 def setup():                                   #setup function
     log.initLogging()                                #setUpLogFile()sets up log file
     log.info('Initializing....')                     #log message
     detector = ip.initImgProsessing()                #initialize blob detector
-    db_ref = db.setupDatabase()                      #setup database
+    db_ref = tel.setupDatabase()                      #setup database
     return detector, db_ref
 
 def main():
-    detector, db_ref = setup()                          #setup function
-    log.info('Setup completed')
-    log.info('Now running main')
+    detector, db_ref = setup()                       #setup function
+    log.info('Setup completed.')
+    log.info('Now running main.')
 
-    
 
     
 if __name__=="__main__":
