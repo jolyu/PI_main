1dff7ea9af15e367441bf0df07e21fae8cd71fa2
sevatm <55739219+sevatm@users.noreply.github.com>
2020-03-23 12:02:47 +0100

    Update telemetry.py

--- telemetry.py
+++ telemetry.py
@@ -6,24 +6,24 @@ import logging as log
 from datetime import datetime
 
 def setupDatabase():
-    cred = credentials.Certificate('key.json')
+    cred = credentials.Certificate('key.json')              #file with API-key and more
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://jolyu-a8615.firebaseio.com/',
         'databaseAuthVariableOverride': {
-        'uid': 'KMz9EwJ5egn5hjAKXd5m7bydVffwnjUz'
-        }})
-    ref = db.reference()
+        'uid': 'KMz9EwJ5egn5hjAKXd5m7bydVffwnjUz'           #uid is used as a 'password' to database
+        }})                                                 #only this uid can write, but all can read
+    ref = db.reference()        #reference to the database we are using
     return ref
 
 def transmit(db_ref, numBlobs, sensorStuff):
         try:
             
             data = {
-                'time': datetime.timestamp(datetime.now()),
+                'time': datetime.timestamp(datetime.now()),     #data to add to database
                 'birds': numBlobs,
             }
-            timestampStr = str(datetime.timestamp(datetime.now()))
-            db_ref.child(timestampStr).set(data)
+            timestampStr = str(datetime.timestamp(datetime.now()))  #use timestamp as database key
+            db_ref.child(timestampStr).set(data)                    #add data to database
             log.info('Uploaded to database')
         except:
             log.critical('Could not upload to database')
