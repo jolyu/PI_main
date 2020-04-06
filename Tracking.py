4f7e5768ac8558a82ce6c81507e0446177c0c664
sevatm <55739219+sevatm@users.noreply.github.com>
2020-04-06 09:32:46 +0200

    Update Tracking.py

--- Tracking.py
+++ Tracking.py
@@ -2,19 +2,24 @@ import cv2
 
 def KeypointsToBoxes(keypoints):
     boxes = []
+    print('transforming keypoints to boxes')
     for keypoint in keypoints:
         point = keypoint.pt
-        size = int(keypoint.size())
+        size = int(keypoint.size)
         box = (int(point[0])- (size/2),int(point[1]) - (size/2),size,size)
         boxes.append(box)
     return boxes
 
 def removeTrackedBlobs(keypoints, boxes):
     newKeypoints = []
+    #print(keypoints)
     diff = []
     try:
+        print('trying')
         for points in keypoints:
-            x,y = points
+            x = points.pt[0]
+            y = points.pt[1]
+            print(str(x) + ", " + str(y))
             for box in boxes:
                 xb,yb,wb,hb = box
                 if xb<x and x<xb+wb and yb<y and y<yb+hb:
