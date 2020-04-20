41b0db70d2bcee3a39a90eb37432ae26b13e063b
SakariaHAli <46136295+SakariaHAli@users.noreply.github.com>
2020-04-20 12:27:39 +0200

    Stuff for testing

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ def main():
     log.info('Setup completed.')
     log.info('Now running main.')
 
-    vc = cv2.VideoCapture(0)                            #start video camera
-    
+    #vc = cv2.VideoCapture(0)                            #start video camera
+    vc = cv2.VideoCapture("video2.avi")     
     if vc.isOpened():                                   #try to get the first frame
         rval, frame = vc.read()   
-        frame = cv2.resize(frame, (650,500))            #frame contains image
+        #frame = cv2.resize(frame, (650,500))            #frame contains image
         try:
             if frame.shape[2]:
             # if there is 3rd dimension
@@ -41,7 +41,7 @@ def main():
     while rval:
         rval, frame = vc.read()                         #read new frame
         cv2.imshow("preview", frame)                    #show image
-        frame = cv2.resize(frame, (650,500))
+        #frame = cv2.resize(frame, (650,500))
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
          
         filteredFrame = ip.filterImg(frame, ip.CV_OTZU_FILTER, ip.MORPHOLOGY_ON) #se globale variabler i image_prosessing.py. midterste er type filter som skal brukes, og siste avgjør om man skal ha morphology operasjoner
@@ -57,6 +57,7 @@ def main():
                 ok = multiTracker.trackers[len(multiTracker.trackers)-1].init(filteredFrame, box) #initialize tracker
         #birds=len(keypoints)
         print(birds)
+        print("test")
         #birds=0
         if cv2.waitKey(30) == 27: # exit on ESC                     #avslutter programmet og lukker alle viduer dersom man trykker ESC
             break
@@ -65,6 +66,5 @@ def main():
     cv2.destroyAllWindows()
     vc.release()
 
-    
 if __name__=="__main__":
     main()
